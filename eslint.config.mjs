f137846fdca904278a3abedb96eb02efbd3fb6e3
nemerK <nemer.k@outlook.com>
2026-01-08 20:33:07 +0200

    Disable prefer-const ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
     },
   },
 ];
